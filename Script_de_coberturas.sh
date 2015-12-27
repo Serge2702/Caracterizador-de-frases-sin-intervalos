56c271c766cfe9a107ba3f7fbbf7f2acde2804ae
Serge2702 <sergiogmv2702@gmail.com>
2015-12-27 17:24:12 -0600

    Modificado para este proyecto

--- Script_de_coberturas.sh
+++ Script_de_coberturas.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Este script se usa automatizar el proceso del cálculo de las cobertura.
 
-for archivo in Resultados/*
+for archivo in "Resultados caracterizador"/*
 do
-    echo "$archivo" >> Resultados/Calculo_coverturas.txt
-    sbcl --script Cobertura.lisp "$archivo" >> Resultados/Calculo_coverturas.txt
+    echo "$archivo" >> "Resultados caracterizador"/Calculo_coverturas.txt
+    sbcl --script Cobertura.lisp "$archivo" >> "Resultados caracterizador"/Calculo_coverturas.txt
 done
