0911098d2e9ba6e454018e9266350c3244fc90a0
Serge2702 <sergiogmv2702@gmail.com>
2015-12-30 13:52:56 -0600

    Arreglando el nombre de los archivos

--- Arreglando_nombres.sh
+++ Arreglando_nombres.sh
@@ -6,6 +6,6 @@ while read linea
 do
     beta=$(echo $linea | cut -f1 -d' ')
     alfa=$(echo $linea | cut -f2 -d' ')
-    echo "alfa: $alfa y beta: $beta"
-    git mv "$beta_-$alfa.txt" "$alfa-$beta.txt"
+    echo "$beta"_-"$alfa.txt -> $alfa-$beta.txt"
+    git mv "$beta"_-"$alfa.txt" "$alfa-$beta.txt"
 done < "$1"
